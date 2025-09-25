57a9e624af242c395e67ebd21525a6d20106a7c6
Iambaio <brauliogarcia@Mac.lan>
2025-09-24 22:56:12 -0700

    Added for loop for steps and time.sleep

--- simulate.py
+++ simulate.py
@@ -1,4 +1,17 @@
 import pybullet as p
+import time
 
-physicsClient = p.connect(p.GUI)
+# Connect to the physics server
+p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+
+
+# Simulation loop
+for i in range(1000):
+    p.stepSimulation()  # Step the physics simulation
+    print(i)  # Print the loop variable
+    # Sleep for 1/60th of a second to slow down the simulation
+    time.sleep(1/60)
+
+# Disconnect from the physics server
 p.disconnect()
